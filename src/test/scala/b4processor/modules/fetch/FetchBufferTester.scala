97add532c52ca1e730d461bea061f0cdaeae8bdd
Hidetaro Tanaka <htanaka@www.nj.cs.tuat.ac.jp>
2023-02-24 15:57:40 +0900

    aaaaa

--- src/test/scala/b4processor/modules/fetch/FetchBufferTester.scala
+++ src/test/scala/b4processor/modules/fetch/FetchBufferTester.scala
@@ -3,6 +3,8 @@ package b4processor.modules.fetch
 import b4processor._
 import chiseltest._
 import org.scalatest.flatspec._
+import chisel3._
+import chisel3.util._
 
 class FetchBufferTester extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "FetchBuffer"
@@ -17,6 +19,11 @@ class FetchBufferTester extends AnyFlatSpec with ChiselScalatestTester {
   it should "not act sussy" in {
     test(new FetchBuffer).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
       // TODO: write some tests to fuck
+      def inputValue(instruction: UInt, programCounter: UInt, index: Int): Unit = {
+        c.io.input.toBuffer(index).bits.instruction.poke(instruction)
+        c.io.input.toBuffer(index).bits.programCounter.poke(programCounter)
+        c.io.input.toBuffer(index).valid.poke(true.B)
+      }
     }
   }
 }
